c659416b933ae15165573df1d5ee3005d62323e7
MH Mahadi <mhmahadi7799@gmail.com>
2021-10-10 08:50:35 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
 import React from "react";
+// import { Component } from "react/cjs/react.production.min";
 import Card from './component/Card'
 import Data from './data'
+import { Component } from 'react'
 
 
 
 
 
-const App = () => {
+// const App = () => {
 
-  const Address = Data.map((item, index) => <Card key={index} CountryName={item.CountryName} CapitalName={item.Capital} City={item.City} />)
+//   const Address = Data.map((item, index) => <Card key={index} CountryName={item.CountryName} CapitalName={item.Capital} City={item.City} />)
 
-  return (
-    <div className='main'>
-      <h1 className='header'>React First App</h1>
-      {Address}
+//   return (
+//     <div className='main'>
+//       <h1 className='header'>React First App</h1>
+//       {Address}
 
-    </div>
-  )
+//     </div>
+//   )
+// }
+
+
+
+
+class App extends Component {
+
+  render() {
+
+    const Address = Data.map((item, index) => <Card key={index} CountryName={item.CountryName} CapitalName={item.Capital} City={item.City} />)
+
+    return (
+      <div className='main'>
+        <h1 className='header'>React First App</h1>
+        {Address}
+
+      </div>
+    )
+  }
 }
+
 
 
 
